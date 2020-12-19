885191898c337c635c98ae4ffc09c34b239c725a
HongBao80 <boy97haiduong@gmail.com>
2020-12-20 02:41:12 +0700

    ngStyle

--- webapp-ui/src/app/app.component.ts
+++ webapp-ui/src/app/app.component.ts
@@ -7,32 +7,8 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  courses = []
-  onAdd() {
-    this.courses.push({ name: "Hix", age: 200 })
-  }
-
-  onRemove(course) {
-    let index = this.courses.indexOf(course)
-    this.courses.splice(index, 1)
-  }
-
-  loadCourses() {
-    this.courses = [
-      { name: "HB", age: 25 },
-      { name: "Trang", age: 21 },
-      { name: "Vui", age: 25 },
-
-    ]
-
-  }
-
-  trackCourse(index, course) {
-    return course ? course.name : undefined
-  }
-
-  onChange(arg) {
-
+  canSave = true
+  onClickSave() {
+    this.canSave = !this.canSave
   }
 }
