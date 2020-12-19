258327cb8e03dd6d19ef3e4f4761bb6c8bdbf868
HongBao80 <boy97haiduong@gmail.com>
2020-12-20 02:24:58 +0700

    load data, track

--- webapp-ui/src/app/app.component.ts
+++ webapp-ui/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FavoriteChangedEventArgs} from './favorite/favorite.component'
+import { FavoriteChangedEventArgs } from './favorite/favorite.component'
 
 @Component({
   selector: 'app-root',
@@ -7,19 +7,27 @@ import {FavoriteChangedEventArgs} from './favorite/favorite.component'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [
-    {name:"HB",age:25},
-    {name:"Trang",age:21},
-    {name:"Vui",age:25},
-
-  ]
-
-  onAdd( ) {
-    this.courses.push({name: "Hix", age: 200})
+  courses = []
+  onAdd() {
+    this.courses.push({ name: "Hix", age: 200 })
   }
 
   onRemove(course) {
-      let index = this.courses.indexOf(course)
-      this.courses.splice(index, 1)
+    let index = this.courses.indexOf(course)
+    this.courses.splice(index, 1)
+  }
+
+  loadCourses() {
+    this.courses = [
+      { name: "HB", age: 25 },
+      { name: "Trang", age: 21 },
+      { name: "Vui", age: 25 },
+
+    ]
+
+  }
+
+  trackCourse(index, course) {
+    return course ? course.name : undefined
   }
 }
