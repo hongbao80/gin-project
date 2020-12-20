653da53ff0bfdfb090b6441cf73b5bec058417af
HongBao80 <boy97haiduong@gmail.com>
2020-12-20 20:24:53 +0700

    Validation error message

---
+++ webapp-ui/src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent  {
+
+  constructor() { }
+
+  log(x) {
+    console.log(x)
+  }
+
+  comment(cmt) {
+    console.log("Comment:", cmt)
+  }
+}
